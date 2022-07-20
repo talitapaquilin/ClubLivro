e9c34b7bb15ef536fde40baab42bd493d70129dd
talitapaquilin <106284355+talitapaquilin@users.noreply.github.com>
2022-07-20 09:54:46 -0300

    Update index.jsx
    Ajuste na redação

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -8,7 +8,7 @@ export function Sidebar() {
       <div className="logo">
         <img src={logo} alt="Logo Book lovers" />
       </div>
-      <p>Esse não é um simples site de livros, e sim um site no qual todos os amantes de livros podem interagir uns com os outros compartilhando da mesma paixão!</p>
+      <p>Este não é um simples site de livros, e sim um ambiente no qual todos os amantes da literatura podem interagir uns com os outros compartilhando da mesma paixão!</p>
     </nav>
   )
 }
